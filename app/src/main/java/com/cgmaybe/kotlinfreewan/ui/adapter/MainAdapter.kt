89eaaf2caa60c341765fea7134d084e3de993295
moubiao <4099587876@qq.com>
2019-02-08 15:02:49 +0800

    主页的adapter使用FragmentPagerAdapter

--- app/src/main/java/com/cgmaybe/kotlinfreewan/ui/adapter/MainAdapter.kt
+++ app/src/main/java/com/cgmaybe/kotlinfreewan/ui/adapter/MainAdapter.kt
@@ -2,10 +2,10 @@ package com.cgmaybe.kotlinfreewan.ui.adapter
 
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.app.FragmentPagerAdapter
 
 class MainAdapter(fragmentManager: FragmentManager, private val mMainData: List<Fragment>) :
-    FragmentStatePagerAdapter(fragmentManager) {
+    FragmentPagerAdapter(fragmentManager) {
 
     override fun getItem(position: Int): Fragment {
         return mMainData[position]
